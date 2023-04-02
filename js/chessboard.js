62ff82519b50d73d98db84efd464d2bfd697df16
vkotu <vkotu@paypal.com>
2023-04-01 19:40:50 -0700

    option4

--- js/chessboard.js
+++ js/chessboard.js
@@ -3,22 +3,29 @@ export default {
 	highlight
 };
 
-var origBoardEl;
-var tiles=[];
+var diagonals = [];
+var tileDiagonals = new Map();
+var highLighted=[];
 // ****************************
 
 function draw(boardEl) {
-	origBoardEl = boardEl;
+	for(let i =0 ; i < 30; i++) {
+		diagonals.push([]);
+	}
+
 	for(let i=0; i< 8; i++) {
 		let rowEl = document.createElement("div");
-		let rowTiles = []
-		tiles.push(rowTiles);
 		for(let j=0;j<8;j++) {
 			let tileEl = document.createElement("div");
-			tileEl.dataset.row = i;
-			tileEl.dataset.col = j;
 			rowEl.appendChild(tileEl);
-			rowTiles.push(tileEl);
+
+			let majorDiag = diagonals[7 - (i-j)];
+			let minorDiag = diagonals[15 + (i+j)];
+
+			majorDiag.push(tileEl);
+			minorDiag.push(tileEl);
+			
+			tileDiagonals.set(tileEl, [majorDiag, minorDiag]);
 		}
 		boardEl.appendChild(rowEl);
 	}
@@ -28,35 +35,20 @@ function draw(boardEl) {
 function highlight(tileEl) {
 	
 	// clear all currently highlighted tiles
-	for (let row of tiles){
-		for(let el of row) {
-			el.classList.remove("highlighted");
+		for(let diag of highLighted) {
+			for(let el of diag) {
+				el.classList.remove("highlighted");
+			}
 		}
-	}
 
 	if(tileEl) {
-		let tileRowIdx = Number(tileEl.dataset.row);
-		let tileColIdx = Number(tileEl.dataset.col);
 
-		//highlight in the up-left direction
-		for(let i = tileRowIdx, j = tileColIdx; i >=0 && j >=0;i--,j--) {
-			let el = tiles[i][j];
-			el.classList.add("highlighted");
-		}
-		//highlight in the up-right direction
-		for(let i = tileRowIdx, j = tileColIdx; i >=0 && j <8;i--,j++) {
-			let el = tiles[i][j];
-			el.classList.add("highlighted");
-		}
-		//highlight in the down-left direction
-		for(let i = tileRowIdx, j = tileColIdx; i <8 && j >=0;i++,j--) {
-			let el = tiles[i][j];
-			el.classList.add("highlighted");
-		}
-		//highlight in the down-right direction
-		for(let i = tileRowIdx, j = tileColIdx; i < 8 && j < 8 ;i++,j++) {
-			let el = tiles[i][j];
-			el.classList.add("highlighted");
+		highLighted = tileDiagonals.get(tileEl);
+
+		for(let diag of highLighted) {
+			for(let el of diag) {
+				el.classList.add("highlighted");
+			}
 		}
 	}
 }
