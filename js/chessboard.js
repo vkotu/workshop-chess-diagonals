08b5a79d74e8af4503d77ee79aa0b407a23ef1ad
vkotu <vkotu@paypal.com>
2023-04-01 19:07:32 -0700

    option3

--- js/chessboard.js
+++ js/chessboard.js
@@ -4,64 +4,59 @@ export default {
 };
 
 var origBoardEl;
-var tiles;
+var tiles=[];
 // ****************************
 
 function draw(boardEl) {
 	origBoardEl = boardEl;
 	for(let i=0; i< 8; i++) {
 		let rowEl = document.createElement("div");
+		let rowTiles = []
+		tiles.push(rowTiles);
 		for(let j=0;j<8;j++) {
 			let tileEl = document.createElement("div");
 			tileEl.dataset.row = i;
 			tileEl.dataset.col = j;
 			rowEl.appendChild(tileEl);
+			rowTiles.push(tileEl);
 		}
 		boardEl.appendChild(rowEl);
 	}
-	tiles = origBoardEl.querySelectorAll("div > div");
+	
  }
 
 function highlight(tileEl) {
 	
 	// clear all currently highlighted tiles
-	for (let el of tiles){
-		el.classList.remove("highlighted");
+	for (let row of tiles){
+		for(let el of row) {
+			el.classList.remove("highlighted");
+		}
 	}
 
 	if(tileEl) {
-		let rowEl = tileEl.parentNode;
-		let tileRowIdx = [...origBoardEl.childNodes].indexOf(rowEl);
-		let tileColIdx = [...rowEl.childNodes].indexOf(tileEl);
+		let tileRowIdx = Number(tileEl.dataset.row);
+		let tileColIdx = Number(tileEl.dataset.col);
 
 		//highlight in the up-left direction
 		for(let i = tileRowIdx, j = tileColIdx; i >=0 && j >=0;i--,j--) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
 		}
 		//highlight in the up-right direction
 		for(let i = tileRowIdx, j = tileColIdx; i >=0 && j <8;i--,j++) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
 		}
 		//highlight in the down-left direction
 		for(let i = tileRowIdx, j = tileColIdx; i <8 && j >=0;i++,j--) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
 		}
 		//highlight in the down-right direction
 		for(let i = tileRowIdx, j = tileColIdx; i < 8 && j < 8 ;i++,j++) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
-		}
-	}
-}
-
-
-function findTile(row,col) {
-	for(let el of tiles) {
-		if(el.dataset.row == row && el.dataset.col == col) {
-			return el;
 		}
 	}
 }
